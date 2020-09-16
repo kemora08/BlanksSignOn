9be0e36694e5148b7c956b117ad689d1f15eb83b
kemora08 <kevin_mora89@hotmail.com>
2020-09-15 21:24:20 -0500

    initial commit

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  formSubmit: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.formSubmit = this.formBuilder.group({
+      inputEmail: [''],
+      inputPassword: ['']
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  submit(): void {
+    console.log(this.formSubmit.value);
   }
 
 }
