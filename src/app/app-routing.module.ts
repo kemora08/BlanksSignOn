4c6922bd0f990ba4e34ce1086ada0d6e079264c2
kemora08 <kevin_mora89@hotmail.com>
2020-09-15 20:39:41 -0500

    add components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: SignInComponent},
+  { path: 'register', component: RegistrationComponent},
+  { path: 'userprofile', component: UserProfileComponent},
+  { path: '**', redirectTo: 'signon', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
