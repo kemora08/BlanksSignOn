249bbfa1d28c1a81d48a94366b46206cc64c2742
kemora08 <kevin_mora89@hotmail.com>
2020-09-16 12:04:32 -0500

    finish authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { RegistrationComponent } from './registration/registration.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -8,7 +9,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 const routes: Routes = [
   { path: 'signin', component: SignInComponent},
   { path: 'register', component: RegistrationComponent},
-  { path: 'userprofile', component: UserProfileComponent},
+  { path: 'userprofile', component: UserProfileComponent, canActivate [AuthGuard] },
   { path: '**', redirectTo: 'signon', pathMatch: 'full'}
 ];
 
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthModule } from './auth.module';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: AuthModule
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService,
+              private authService: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (this.authService.loggedIn) {
     return true;
+    }
+
+    this.router.navigate(['/signin']);
+
+    return false;
   }
-  
+
 }
