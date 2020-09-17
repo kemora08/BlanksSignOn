fd65460745cdaab6f731cd3244225dbd6519f945
kemora08 <kevin_mora89@hotmail.com>
2020-09-16 20:20:31 -0500

    initial commit

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -7,23 +8,23 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
 
   formSubmit: FormGroup;
   inputEmail: FormControl = new FormControl('', Validators.email);
-  inputPassword: FormControl = new FormControl('', Validators.required); )
+  inputPassword: FormControl = new FormControl('', Validators.required);
   validLogin = true;
 
-  get inputPasswordInvalid(): boolean {
-    return !this.inputPassword.valid && this.inputPassword.touched;
+  get inputEmailInvalid(): boolean {
+    return !this.inputEmail.valid && this.inputEmail.touched;
   }
 
   get inputPasswordInvalid(): boolean {
     return !this.formSubmit.controls.inputPassword.valid &&
     this.formSubmit.controls.inputPassword.touched;
   }
-  constructor(private formBuilder: FormBuilder) {
-    private authService: AuthService; )
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+
 
 this.formSubmit = this.formBuilder.group({
       inputEmail: ['', Validators.email],
@@ -31,23 +32,19 @@ this.formSubmit = this.formBuilder.group({
     });
    }
 
-ngOnInit(); : void {
-  };
 
-submit(); : void {
+submit(): void {
   console.log(this.formSubmit.value);
 
   this.validLogin = this.authService.login(this.formSubmit.controls.inputEmail.value,
     this.formSubmit.controls.inputPassword.value);
 
-    const user: IUser = {
-     username: this.inputEmail.value,
-     password: this.inputPassword.value
-    };
+  //const user: IUser = {
+    // username: this.inputEmail.value,
+    //password: this.inputPassword.value
+   // };
 
-  if(this.validLogin); {
+  if (this.validLogin) {
     this.router.navigate(['/userProfile']);
   }
-
-
-}
+}}
